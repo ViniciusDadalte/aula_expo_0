afb716e36550b736d885799d2e4021a09924a2bc
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-02-27 15:24:12 -0300

    aula 27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+function exemplo_0 () {
+    return(
+        <View>
+            <Text>Hello wolrd! :D</Text>
+        </View>
+    );
+}
+export default exemplo_0;
