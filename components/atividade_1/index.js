f5d338d1ec2ff6c1527bb93c85c672f70251a43e
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-03-12 15:24:17 -0300

    aula-12-03-24
    mudei criei outro exemplo

---
+++ components/atividade_1/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+function Atividade_1(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.texto1}>Atividade 1</Text>
+            <Text style={styles.texto2}>Vinicius Gabriel</Text>
+        </View>
+    );
+}
+export default Atividade_1;
---
+++ components/atividade_1/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'lightgreen',
+        padding: 10,
+        borderRadius:20,
+    },
+    texto1:{
+        color:'darkgreen',
+        fontSize:20,
+        fontWeight:'bolder',
+    },
+    texto2:{
+        color:'black',
+        fontSize:20,
+        fontWeight:'bold',
+    }
+});
+
+export default styles;
