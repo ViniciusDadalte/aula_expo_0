9011d8e494ac785dcd20cd3a2e76bcc49610e8f0
Vinicius Gabriel <156528276+ViniciusDadalte@users.noreply.github.com>
2024-07-26 09:14:04 -0300

    apaguei linhas desnecessarias
    oii ewerton :D

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Exemplo_0 from './components/exemplo_1';
-import Atividade_1 from './components/atividade_1';
-import Exemplo_2 from './components/exemplo_2';
-import Atividade_2 from './components/atividade_2';
-import Exemplo_3 from './components/exemplo_3';
-import Atividade_3 from './components/atividade_3';
 import Exemplo_4 from './components/exemplo_4';
 
 export default function App() {
